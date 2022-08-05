919565e6388d33fc6dc61e172fff178c2977552b
Ian <ianwebikirda@gmail.com>
2022-08-05 23:32:45 +0700

    routes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 // parse appclication json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Panggil Routes 
+var routes = require('./routes');
+routes(app);
 app.listen(666, () => {
     console.log(`Server started on port`);
 });
