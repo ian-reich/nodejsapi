082d58ab947244f68ad4e7eb6b12d69bded2b905
Ian <ianwebikirda@gmail.com>
2022-08-06 19:00:16 +0700

    Get All Data API

--- controller.js
+++ controller.js
@@ -5,3 +5,13 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API Berhasil Dijalankan", res);
 }
+// Fecth All Data 
+exports.getAllData = function(req,res){
+    connection.query("SELECT * FROM tm_komik", function(error, rows, fields){
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = function (app) {
     var jsonku = require('./controller');
+
+    // Index URL Path 
     app.route('/')
         .get(jsonku.index);
-}
+    
+    // Get All Data 
+    app.route('/getalldata')
+        .get(jsonku.getAllData);
+};
